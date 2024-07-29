9f917fda2f9920735b50dc6ac92414773712e0a4
Rafal Rudnicki <rafal.rudnicki@intel.com>
2024-07-29 16:39:48 +0200

    add CUDA provider

---
+++ examples/common/utils_cuda.h
@@ -0,0 +1,19 @@
+/*
+ *
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ */
+
+#ifndef UMF_EXAMPLE_UTILS_CUDA_H
+#define UMF_EXAMPLE_UTILS_CUDA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// To use the CUDA API, the CUDA SDK has to be installed on the system
+#include "cuda.h"
+
+#endif // UMF_EXAMPLE_UTILS_CUDA_H
